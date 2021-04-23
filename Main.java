e80f5dca3d57ff4b1ef84bc378208a89ddcc1fbf
daniel5115 <>
2021-04-23 14:15:09 +0000

    all

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
-class Main {
+class Main 
+{
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
     AdjListGraph g = new AdjListGraph();
